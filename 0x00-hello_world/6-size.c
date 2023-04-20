76c94de9b988d1ae3fbd79e6b1fc3c9516d8c8b8
Abiolaolayiwola <abiolaoluwalonimi@gmail.com>
2023-04-20 12:02:16 -0700

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints the various types of computer
- * Return 0 (Success)
- */
+ * main - Entry point
+ *
+ * Description: A c program that prints the size of various types on the computer.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char a;
-	int b;
-	long int 
-
+	printf("size of a char: %lu byte(s)\n",sizeof(char));
+	printf("size of a int: %lu byte(s)\n",sizeof(int));
+	printf("size of a long int: %lu byte(s)\n",sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n",sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n",sizeof(float));
+	return (0)
+}
