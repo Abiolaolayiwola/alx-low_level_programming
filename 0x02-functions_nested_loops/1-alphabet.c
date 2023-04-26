b3a8d48396ec9303f5ce5758b601524fbd38bccc
Abiolaolayiwola <abiolaoluwalonimi@gmail.com>
2023-04-26 03:50:00 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * print_alphabet - make the alphabet
+ * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
 
