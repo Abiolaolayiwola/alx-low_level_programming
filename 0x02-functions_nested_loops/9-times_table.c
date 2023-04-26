263405441105c0e2fb8a7b6186f84bddfcc26240
Abiolaolayiwola <abiolaoluwalonimi@gmail.com>
2023-04-26 04:01:32 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,9 +5,11 @@
  * Return: Empty output
  */
 
-void times_table(void);
-}
+void times_table(void)
+{
+
 	int x, y, z, u, d;
+
 	for (x = 0; x <= 9; x++)
 	{
 	for (y = 0; y <= 9; y++)
@@ -17,7 +19,6 @@ void times_table(void);
 	{
 	u = z % 10;
 	d = (z - u) / 10;
-	{
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
