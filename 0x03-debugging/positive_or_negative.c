c6dcc17f8c6e414a3b637e8cdded091f9ebd92ed
Abiolaolayiwola <abiolaoluwalonimi@gmail.com>
2023-04-28 08:13:07 -0700

    i-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Abiola code
+ * Description: "determine if a integer is positive or negative,zero
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+	return (0);
+}
