fd8c8493337fc5d64858a74bdd0c11fda0e3340f
Abiolaolayiwola <abiolaoluwalonimi@gmail.com>
2023-05-17 14:04:18 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -12,8 +12,9 @@
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
+
 	i = 0;
-       	while (i < size)
+	while (i < size)
 	{
 		if (i % 10)
 		{
@@ -33,10 +34,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  *
  * Return: Always 0.
  */
-int main(void)
 
+int main(void)
+{
 	char *buffer;
-	
+
 	buffer = create_array(98, 'H');
 if  (buffer == NULL)
 {
